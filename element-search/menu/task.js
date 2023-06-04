3e8c6bd8a8db414f3a4093e6c4fe4a2b827c9038
Gremelya <115106904+Gremelya@users.noreply.github.com>
2023-06-04 11:47:38 +0300

    Update task.js
    на проверку

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,6 @@
-const links = document.querySelectorAll('.menu__link');
-
-for (let link of Array.from(links)) {
-    link.onclick = function () {
-
-        const parent = link.parentElement;
-        if (parent.querySelector('.menu_sub').className === 'menu menu_sub') {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub menu_active';
-        } else {
-            parent.querySelector('.menu_sub').className = 'menu menu_sub'
-        }
-
-        if (link.closest('.menu_main')) {
-            return false
-        }
+document.querySelector(".menu_main").onclick = (event) => {
+    if (event.target.nextElementSibling) {
+      event.target.nextElementSibling.classList.toggle("menu_active");
+      return false;
     }
-}
+  };
